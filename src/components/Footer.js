eae1945fd504a76776faf478419857dafb1339e7
Asitmondal <mondalasit973@gmail.com>
2023-09-06 15:15:33 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer className='bg-primary py-12'>
+    <div className='container mx-auto'>
+      <pc className='text-white text-center'> Copyright &copy; Asit 2023. All right resrved.</pc>
+    </div>
+  </footer>;
+};
+
+export default Footer;
